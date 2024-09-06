7edca929799b626b8f9bb1b613492061d56a1e63
Satvik-Verma <65223530+Satvik-Verma@users.noreply.github.com>
2024-09-06 15:33:57 -0700

    updated sum_indirect setup

--- cp1/sum_indirect.cpp
+++ cp1/sum_indirect.cpp
@@ -12,21 +12,26 @@ void
 setup(int64_t N, uint64_t A[])
 {
    printf(" inside sum_indirect problem_setup, N=%lld \n", N);
-   for(int i=0; i<N; i++){
-      A[i] = std::rand();
+   std::random_device rd;
+   std::mt19937 gen(rd());
+   std::uniform_int_distribution<uint64_t> dis(0, N-1);
+
+    // Fill array A with random values
+   for (int64_t i = 0; i < N; ++i) {
+      A[i] = dis(gen);
    }
 }
 
 int64_t
 sum(int64_t N, uint64_t A[])
 {
-   int64_t sum = 0;
-   uint64_t indx = 0;
+    int64_t sum = 0;
 
-    while (indx < N) {
-        sum += A[indx];
-        indx = A[indx];
+    // Compute the sum accumulating A[indx] where indx = A[indx]
+    for (int64_t i = 0; i < N; ++i) {
+        sum += A[A[i]];
     }
+
    printf(" inside sum_indirect perform_sum, N=%lld \n", N);
 
    return sum;
